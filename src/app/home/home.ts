8571915416f74e45c2059b17dcce35b3efd879ce
Adelakun Abayomi <adelakunabayomi123@gmail.com>
2023-03-23 10:08:15 +0100

    first commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,6 @@
+export interface Skill {
+    frontEnd: string;
+    backEnd: string;
+    myTools: string;
+    myOthers: string;
+}
---
+++ src/app/notfound/notfound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cv-notfound',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.css']
+})
+export class NotfoundComponent {
+
+}
